541075794256eddf20b3ef684ac6243e584fd128
Vijay Tangub <71299932+SoneVijay@users.noreply.github.com>
2022-01-13 17:10:57 +0800

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://unicons.iconscout.com/release/v2.1.9/css/unicons.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body style="background-color: #e8e4c9;">
+<!-- partial:index.partial.html -->
+	<div class="section">
+		<div class="container">
+			<div class="row full-height justify-content-center">
+				<div class="col-6 text-center align-self-center py-5">
+					<img width = 400 src="logo.png"/>
+				</div>
+					<div class="col-6 text-center align-self-center py-5">
+					<div class="section pb-5 pt-1 pt-sm-2 text-center">
+						<div class="card-3d-wrap mx-auto">
+							<div class="card-3d-wrapper">
+								<div class="card-front">
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Sign Up</h4>
+											<div class="form-group">
+												<input type="text" name="logname" class="form-style" placeholder="First Name" id="logname" autocomplete="off">
+												<i class="input-icon uil uil-user"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="text" name="logname" class="form-style" placeholder="Last Name" id="logname" autocomplete="off">
+												<i class="input-icon uil uil-user"></i>
+											</div>		
+											<div class="form-group mt-2">
+												<input type="email" name="logemail" class="form-style" placeholder="Email" id="logemail" autocomplete="off">
+												<i class="input-icon uil uil-at"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Password" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<div class="form-group mt-2">
+												<input type="password" name="logpass" class="form-style" placeholder="Confirm Password" id="logpass" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+											<a href="#" class="btn mt-4">sign up</a>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
